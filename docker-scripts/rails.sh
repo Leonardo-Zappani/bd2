4cd3d794216bebe50afd8210f1d08c5fe4ffea71
Leonardo Fernandes Zappani <102327012+Leonardo-Zappani@users.noreply.github.com>
2023-11-30 19:29:11 -0300

    fixes and improvements

--- docker-scripts/rails.sh
+++ docker-scripts/rails.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
 # uncomment lines 3-11 to create your db
-# echo "create pg database"
-# bundle exec rake db:create
+ echo "create pg database"
+ bundle exec rake db:create
 
-# echo "run rails db migradtions"
-# bundle exec rake db:migrate
+ echo "run rails db migradtions"
+ bundle exec rake db:migrate
 
-# echo "seed database"
-# bundle exec rake db:seed
-
-# echo "Running BUNDLE INSTALL for rails..."
-# bundle install
+ echo "Running BUNDLE INSTALL for rails..."
+ bundle install
 
 echo "Starting rails app server ..."
 bin/dev
